b3aff343ab76a7600344a4fcd92296e4c8330ffb
yushe629 <58897653+yushe629@users.noreply.github.com>
2022-06-22 19:05:24 +0900

    WIP: use message_filter

--- turtlebot3_explore/scripts/get_co2.py
+++ turtlebot3_explore/scripts/get_co2.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python
 import rospy
 from std_msgs.msg import Float32, UInt16
-
+from turtlebot3_explore.msg import Gas
 
 class get_co2:
     def __init__(self):
         
         rospy.init_node("get_co2")
-        self.gas_pub = rospy.Publisher("/gas", Float32, queue_size=10)
+        self.gas_pub = rospy.Publisher("/gas", Gas, queue_size=10)
         self.eco2_sub = rospy.Subscriber("/eco2", UInt16, self.callback)
         self.norm = 10.0
         rospy.spin()
         
     def callback(self, msg):
-        gas_msg = Float32()
-        gas_msg.data = float(msg.data)/self.norm
+        gas_msg = Gas()
+        gas_msg.header.stamp = rospy.Time.now()
+        gas_msg.gas.data = float(msg.data)/self.norm
         self.gas_pub.publish(gas_msg)
 
 if __name__ == "__main__":
