ffa02578ebec3870e78b7bb2edf06a918f1be2b8
yushe629 <58897653+yushe629@users.noreply.github.com>
2022-06-24 17:37:38 +0900

    set params

--- turtlebot3_explore/scripts/gas_scrutinize.py
+++ turtlebot3_explore/scripts/gas_scrutinize.py
@@ -12,16 +12,16 @@ import numpy as np
 from turtlebot3_explore.msg import Gas
 
 # if scan_val is Inf, inf_distance is assigned.
-inf_distance = 5.0
+# inf_distance = 5.0
 # the radius of the robot explore area
 # territory_radius = 1.0  for aibot
-half_of_scan_size = 20
+# half_of_scan_size = 20
 # the velocity of exploring and the type of explore state
-explore_vel = 0.3
-explore_time = 0.3
-explore_yaw_vel = 1.0
-explore_yaw_time = 0.3
-# explore_state = ['front', 'back', 'turn', 'after_turn', 'explored'] 
+# explore_vel = 0.3
+# explore_time = 0.3
+# explore_yaw_vel = 1.0
+# explore_yaw_time = 0.3
+# explore_state = ['front', 'back', 'turn', 'after_turn', 'explored']
 
 class gas_scrutinize:
     def __init__(self):
@@ -42,8 +42,15 @@ class gas_scrutinize:
         # params
         self.territory_radius = rospy.get_param("~territory_radius", 0.6)
         self.timeout_sec = rospy.get_param("~timeout_sec", 15.0)
+        self.inf_distance = rospy.get_param("~inf_distance", 5.0)
+        # half counts of Lasascan.data for using
+        self.half_of_scan_size = rospy.get_param("~half_of_scan_size", 20)
+        self.explore_vel_x = rospy.get_param("~explore_vel_x", 0.3)
+        self.explore_vel_yaw = rospy.get_param("~explore_vel_yaw", 0.3)
+        self.explore_time_x = rospy.get_param("~explore_time_x", 1.0)
+        self.explore_time_yaw = rospy.get_param("~explore_time_yaw", 0.3)
 
-        self.execute = False
+        self.execute = True
         self.start_time = 0
         self.max_gas_value = 0.0
         self.final_robot_pose = Pose()
@@ -52,9 +59,9 @@ class gas_scrutinize:
         self.cmd_x = 0.0
         self.cmd_yaw = 0.0
         self.explore_state = 'front'
-        self.iscorrect_path = True
+        self.iscorrect_path = False
         self.max_gas_value_time = rospy.get_time()
-        self.before_gas_value = 0
+        self.before_gas_value = 0.0
 
         rospy.spin()
 
@@ -75,20 +82,21 @@ class gas_scrutinize:
 
     def explore(self):
         if self.explore_state == 'front':
-            self.cmd_x = explore_vel
+            self.cmd_x = self.explore_vel_x
             self.cmd_yaw = 0.0
             self.explore_state = 'back'
         elif self.explore_state == 'back':
-            rospy.sleep(explore_time)
-            self.cmd_x = -1.0*explore_vel
+            rospy.sleep(self.explore_time_x)
+            self.cmd_x = -1.0*self.explore_vel_x
+            self.cmd_yaw = 0.0
             self.explore_state = 'turn'
         elif self.explore_state == 'turn':
-            rospy.sleep(explore_time)
+            rospy.sleep(self.explore_time_x)
             self.cmd_x = 0.0
-            self.cmd_yaw = explore_yaw_vel
+            self.cmd_yaw = self.explore_vel_yaw
             self.explore_state = 'after_turn'
         elif self.explore_state == 'after_turn':
-            rospy.sleep(explore_yaw_time)
+            rospy.sleep(self.explore_time_yaw)
             self.cmd_x = 0.0
             self.cmd_yaw = 0.0
             self.explore_state = 'front'
@@ -122,10 +130,10 @@ class gas_scrutinize:
             return
 
         size = len(scan_msg.ranges)
-        front_dists = scan_msg.ranges[:half_of_scan_size]+ scan_msg.ranges[(size - half_of_scan_size):]
-        front_dist = min(list(map(lambda x: inf_distance if (x == float('inf') or x == 0.0
+        front_dists = scan_msg.ranges[:self.half_of_scan_size]+ scan_msg.ranges[(size - self.half_of_scan_size):]
+        front_dist = min(list(map(lambda x: self.inf_distance if (x == float('inf') or x == 0.0
         ) else x, front_dists)))
-        # rospy.loginfo_throttle(1.0, "minimum front distance: %f", front_dist) 
+        # rospy.loginfo_throttle(1.0, "minimum front distance: %f", front_dist)
 
         if self.before_gas_value < gas_msg.gas.data:
             self.explore_state = 'front'
@@ -134,6 +142,7 @@ class gas_scrutinize:
             # Avoiding obstacle
             self.cmd_x = 0.0
             self.cmd_yaw = 0.5
+            self.explore_state = "front"
             rospy.loginfo_throttle(1.0, 'state: avoiding obstacle',)
         else:
             self.cmd_yaw = 0.0
